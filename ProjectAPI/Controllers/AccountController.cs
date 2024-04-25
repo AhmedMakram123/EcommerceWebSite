d988a362234435517fbe4d9f9252aba29a890fa8
limitlesswill <wa.elose@gmail.com>
2024-04-25 18:38:58 +0200

    - getRole endpoint - user disallowed to have multiple email fix - seller error fix - customer error fix - admin error fix - register error fix

--- ProjectAPI/Controllers/AccountController.cs
+++ ProjectAPI/Controllers/AccountController.cs
@@ -36,19 +36,30 @@ namespace ProjectAPI.Controllers
         {
             if (ModelState.IsValid)
             {
+                // WAEL FIX - CHECK IF EMAIL EXISTS IN DATABASE
+                var userExists = await usermanger.FindByEmailAsync(userDto.email);
+                if (userExists != null)
+                    return StatusCode(500, "Email already Exist & cannot be reused");
                 //save
                 ApplicationUser user = new ApplicationUser();
                 user.UserName = Guid.NewGuid().ToString();
                 user.fName = userDto.fName;
-                user.lName = userDto.lName; 
+                user.lName = userDto.lName;
                 user.PasswordHash = userDto.password;
                 user.PhoneNumber = userDto.PhoneNumber;
                 user.Email = userDto.email;
                 IdentityResult result = await usermanger.CreateAsync(user, userDto.password);
-                if (result.Succeeded)
+                try
                 {
-                    await usermanger.AddToRoleAsync(user, "Admin");
-                    return Ok("Admin Add Success");
+                    if (result.Succeeded)
+                    {
+                        await usermanger.AddToRoleAsync(user, "Admin");
+                        return Ok("Admin Add Success");
+                    }
+                }
+                catch
+                {
+                    return StatusCode(500, "Admin Role doesn't exists");
                 }
                 return BadRequest(result.Errors.FirstOrDefault());
             }
@@ -61,26 +72,37 @@ namespace ProjectAPI.Controllers
         {
             if (ModelState.IsValid)
             {
+                // WAEL FIX - CHECK IF EMAIL EXISTS IN DATABASE
+                var userExists = await usermanger.FindByEmailAsync(userDto.email);
+                if (userExists != null)
+                    return StatusCode(500, "Email already Exist & cannot be reused");
                 //save
                 ApplicationUser user = new ApplicationUser();
                 user.UserName = Guid.NewGuid().ToString();
                 user.fName = userDto.fName;
-                user.lName = userDto.lName; 
+                user.lName = userDto.lName;
                 user.PasswordHash = userDto.password;
                 user.PhoneNumber = userDto.PhoneNumber;
                 user.Email = userDto.email;
                 IdentityResult result = await usermanger.CreateAsync(user, userDto.password);
-                if (result.Succeeded)
+                try
                 {
-                    await usermanger.AddToRoleAsync(user, "Seller");
-                    return Ok("Seller Add Success");
-                }
-                else
-                {
-                    foreach (var item in result.Errors)
+                    if (result.Succeeded)
                     {
-                        return BadRequest(item);
+                        await usermanger.AddToRoleAsync(user, "Seller");
+                        return Ok("Seller Add Success");
                     }
+                    else
+                    {
+                        foreach (var item in result.Errors)
+                        {
+                            return BadRequest(item);
+                        }
+                    }
+                }
+                catch
+                {
+                    return StatusCode(500, "Seller Role doesn't exists");
                 }
             }
             return BadRequest(ModelState);
@@ -91,26 +113,38 @@ namespace ProjectAPI.Controllers
         {
             if (ModelState.IsValid)
             {
+                // WAEL FIX - CHECK IF EMAIL EXISTS IN DATABASE
+                var userExists = await usermanger.FindByEmailAsync(userDto.email);
+                if (userExists != null)
+                    return StatusCode(500, "Email already Exist & cannot be reused");
                 //save
                 ApplicationUser user = new ApplicationUser();
                 user.UserName = Guid.NewGuid().ToString();
-                user.fName= userDto.fName;
-                user.lName= userDto.lName;
+                user.fName = userDto.fName;
+                user.lName = userDto.lName;
                 user.PasswordHash = userDto.password;
                 user.PhoneNumber = userDto.PhoneNumber;
                 user.Email = userDto.email;
                 IdentityResult result = await usermanger.CreateAsync(user, userDto.password);
-                if (result.Succeeded)
+
+                try
                 {
-                    //await usermanger.AddToRoleAsync(user, "Customer");
-                    return Ok(new { message = "Customer Add Success" });
-                }
-                else
-                {
-                    foreach (var item in result.Errors)
+                    if (result.Succeeded)
                     {
-                        return BadRequest(item);
+                        await usermanger.AddToRoleAsync(user, "Customer");
+                        return Ok(new { message = "Customer Add Success" });
                     }
+                    else
+                    {
+                        foreach (var item in result.Errors)
+                        {
+                            return BadRequest(item);
+                        }
+                    }
+                }
+                catch
+                {
+                    return StatusCode(500, "Customer Role doesn't exists");
                 }
             }
             return BadRequest(ModelState);
@@ -122,30 +156,42 @@ namespace ProjectAPI.Controllers
         {
             if (ModelState.IsValid == true)
             {
-                //check - create token
-                ApplicationUser user = await usermanger.FindByEmailAsync(userDto.UserEmail);
+                //check - create token WAEL FIX
+                ApplicationUser user = null;
+                try
+                {
+                    user = await usermanger.FindByEmailAsync(userDto.UserEmail);
+                }
+                catch
+                {
+                    return StatusCode(500, $"DUPLICATE EMAILS in DATABASE");
+                }
                 if (user != null)//user name found
                 {
                     bool found = await usermanger.CheckPasswordAsync(user, userDto.Password);
                     if (found)
                     {
+
                         //Claims Token
                         var claims = new List<Claim>();
                         claims.Add(new Claim(ClaimTypes.Name, user.UserName));
                         claims.Add(new Claim(ClaimTypes.NameIdentifier, user.Id));
                         claims.Add(new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()));
 
-                        //get role
                         var roles = await usermanger.GetRolesAsync(user);
+
+
                         foreach (var itemRole in roles)
                         {
                             claims.Add(new Claim(ClaimTypes.Role, itemRole));
                         }
+
                         SecurityKey securityKey =
                             new SymmetricSecurityKey(Encoding.UTF8.GetBytes(config["JWT:Secret"]));
 
                         SigningCredentials signincred =
                             new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
                         //Create token
                         JwtSecurityToken mytoken = new JwtSecurityToken(
                             issuer: config["JWT:ValidIssuer"],//url web api
@@ -167,6 +213,33 @@ namespace ProjectAPI.Controllers
             return Unauthorized();
         }
 
+        [HttpPost("getRole")]//api/account/getRole
+        public async Task<IActionResult> getRole(LoginUserDto userDto)
+        {
+            if (ModelState.IsValid == false)
+                return Unauthorized("Data entered is incorrect");
 
+            ApplicationUser user = null;
+            try
+            {
+                user = await usermanger.FindByEmailAsync(userDto.UserEmail);
+            }
+            catch
+            {
+                return StatusCode(500, $"DUPLICATE EMAILS in DATABASE");
+            }
+
+            if (user is null)
+                return Unauthorized("User not found");
+
+            bool found = await usermanger.CheckPasswordAsync(user, userDto.Password);
+
+            if (!found)
+                return Unauthorized("username or password is incorrect");
+
+            var roles = await usermanger.GetRolesAsync(user);
+
+            return Ok(roles);
+        }
     }
 }
